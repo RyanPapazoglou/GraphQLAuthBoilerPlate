b7935aa88224f712da05eaf110f1dc7d973dad83
ryan papazoglou <r.papazoglou@gmail.com>
2020-10-27 23:03:25 -0400

    Added README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Boiler plate code for GrapgQL Auth
+### Using/To be used:
+ - mongodb
+ - express
+ - node
+ - react
+ - graphql
--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGODB: 'mongodb+srv://ryan:rJbp1pKbZjgJsK4t@cluster0.tma8u.mongodb.net/merng?retryWrites=true&w=majority',
-    SECRET_KEY: 'VERY_SECRET'
-}
